28cb8d8c8147a4b92f0f557d28cb57fe3c4073ad
Sam Hartling <sam.hartling@mail.utoronto.ca>
2026-03-03 09:50:14 -0500

    Fixed Coworker

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,6 +51,8 @@ cp ./data/raw/user_log_*.log ./data/processed/user_logs
 cp ./data/raw/event_log_*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm ./data/raw/ipaddr_*.txt
+rm ./data/raw/user_ipaddr_*.log
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt 
